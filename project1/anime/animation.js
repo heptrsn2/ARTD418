e31925f003b89ecb3ad95ce8ecf38d95230df8c9
heptrsn2 <60363903+heptrsn2@users.noreply.github.com>
2020-02-17 14:44:55 -0600

    new pages

---
+++ project1/anime/animation.js
@@ -0,0 +1,42 @@
+// By Class
+anime({
+  targets: '.image1',
+  translateX: 90,
+  translateY: 90,
+  easing: 'easeInOutQuad'
+})
+
+anime({
+  targets: '.image2',
+  translateX: 300,
+  translateY: -150,
+  easing: 'easeInOutQuad',
+})
+
+anime({
+  targets: '.image3',
+  translateX: 380,
+  translateY: -140,
+  easing: 'easeInOutQuad',
+})
+
+anime({
+  targets: '.image4',
+  translateX: 40,
+  translateY: -200,
+  easing: 'easeInOutQuad'
+})
+
+anime({
+  targets: '.image5',
+  translateX: 400,
+  translateY: -300,
+  easing: 'easeInOutQuad'
+})
+
+anime({
+  targets: '.text',
+  translateX: 800,
+  translateY: -600,
+  easing: 'easeInOutQuad'
+})
