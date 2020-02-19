a5cb5b4d6c00a35c05cf9b0aa2df723cd130293e
heptrsn2 <60363903+heptrsn2@users.noreply.github.com>
2020-02-19 11:22:11 -0600

    Geometric animation :)

---
+++ project1/anime/js/geometry.js
@@ -0,0 +1,69 @@
+// By Class
+anime({
+  targets: '.box1',
+  translateX: 245,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  scale: 2,
+  rotate: 360
+})
+
+anime({
+  targets: '.box2',
+  translateY: 300,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  scale: 2,
+  rotate: 180
+})
+
+anime({
+  targets: '.box3',
+  translateY: -300,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  scale: 2,
+  rotate: -360
+})
+
+anime({
+  targets: '.box4',
+  translateX: -300,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  scale: 2,
+  rotate: -180
+})
+
+anime({
+  targets: '.box5',
+  translateY: -300,
+  translateX: 500,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  scale: 2
+})
+
+anime({
+  targets: '.box6',
+  translateY: -300,
+  translateX: -100,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  scale: 2
+})
+
+anime({
+  targets: '.box7',
+  translateY: -300,
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  scale: 2
+})
