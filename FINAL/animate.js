c5622f457b09697e4951c22fff6886db2faab1b7
heptrsn2 <60363903+heptrsn2@users.noreply.github.com>
2020-04-21 21:49:26 -0500

    New animations

--- FINAL/animate.js
+++ FINAL/animate.js
@@ -3,8 +3,17 @@ var elements = document.querySelectorAll("polygon");
 anime ({
   targets: elements,
   baseFrequency: 0,
-  scale: (-2),
+  scale: function() { return anime.random(10,20) / 10; },
   easing: 'easeInOutQuad',
   direction: 'alternate',
   loop: true
+})
+
+anime ({
+  targets: 'circle',
+  opacity: function() { return anime.random(0,10) / 10; },
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true,
+  duration: 800s
 })
