6c5110d5e744a0f6196ba74bf8556d9b3dcb36fc
heptrsn2 <60363903+heptrsn2@users.noreply.github.com>
2020-04-22 17:06:23 -0500

    Updates, horizontal scroll

--- FINAL/animate.js
+++ FINAL/animate.js
@@ -1,19 +1,67 @@
 var elements = document.querySelectorAll("polygon");
 
 anime ({
-  targets: elements,
+  targets:'.red',
   baseFrequency: 0,
-  scale: function() { return anime.random(10,20) / 10; },
+  scale: function() { return anime.random(12,15) / 10; },
   easing: 'easeInOutQuad',
   direction: 'alternate',
   loop: true
-})
+});
 
 anime ({
-  targets: 'circle',
-  opacity: function() { return anime.random(0,10) / 10; },
+  targets:'.renewal',
+  rotate: 360,
+  easing: 'linear',
+  loop: true,
+  duration: 12000
+});
+
+anime ({
+  targets:'line',
+  keyframes: [
+    {scale: 0.5},
+    {scale: 3}
+  ],
+  scale: 3,
+  easing: 'easeInOutQuad',
+  duration: 8000,
+  loop: true
+});
+
+anime ({
+  targets: pull,
+  baseFrequency: 0,
+  scale: function() { return anime.random(12,15) / 10; },
   easing: 'easeInOutQuad',
   direction: 'alternate',
-  loop: true,
-  duration: 800s
-})
+  loop: true
+});
+
+var pull = document.querySelectorAll("circle");
+
+anime({
+            targets: pull,
+            translateY: anime.stagger(1500, {
+                grid: [10, 18],
+                from: 'center',
+                axis: 'y'
+            }),
+            delay: anime.stagger(100, {
+                start: 200,
+                grid: [10, 18],
+                from: 'center'
+            }),
+            easing: 'easeInOutQuad',
+            direction: 'reverse',
+            loop: true
+        });
+
+//anime ({
+  //targets: 'circle',
+  //opacity: function() { return anime.random(0,10) / 10; },
+  //easing: 'easeInOutQuad',
+  //direction: 'alternate',
+  //loop: true,
+  //duration: 800
+//})
